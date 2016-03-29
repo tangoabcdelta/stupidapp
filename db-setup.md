a31234fda98b243fcc9130348a64392c6aed6aba
Deveedutta Maharana <deveedutta@Deveeduttas-iMac.local>
2016-03-30 04:56:20 +0530

    basdjsdlkajsdlasj

---
+++ db-setup.md
@@ -0,0 +1,14 @@
+Want to
+* Import a `properly structured` json object into a table in a `relational database`
+* Import json data into a `collection` in a `no-sql database`
+* Define or create schema as a grunt-task
+
+This task helps you in achieving the above.
+
+It uses knex and lodash as dependencies.
+* lodash to read the json in recursion
+* knex as the ORM
+  * to write into a db provided in the config
+  * to define new schema
+
+
--- grunt/dbsetup.js
+++ grunt/dbsetup.js
@@ -1,5 +1,90 @@
-module.exports = function ( grunt ) {
-  grunt.registerTask('dbsetup', function( targetEnvironment ) {
-    
-  });
+var _ = require('lodash');
+var path = require("path");
+var knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host     : '127.0.0.1',
+    user     : 'deveedutta',
+    password : '',
+    database : 'sattabazi'
+    // ,
+    // debug    : true
+  }
+});
+
+
+var sampleDataPath = path.join(__dirname, "../dbfixtures/events.js");
+var sampledata = require(sampleDataPath);
+
+
+var config = {
+  tableName: "sutta",
+  logQueries: true,
+  ifNotExistsOverride: true,
 }
+
+
+var msg = "";
+
+
+
+
+
+// knex.select().from('sutta')
+// .then(function(data){
+//   data = JSON.stringify(data);
+//   console.log( data );
+//   grunt.log.writeln('All done!');
+//   done();
+// });
+
+
+
+
+var fields = Object.keys( sampledata[0] );
+console.log("fields:", fields);
+
+knex.schema
+.dropTableIfExists( config.tableName )
+.then(function(){
+
+  knex.schema
+  .createTable( config.tableName, function (table) {
+    _.each( fields, function( item ){
+      table.string( item );
+      console.log( "field-name:", item );
+    })
+
+
+  })
+  .then(function(){
+
+    knex( config.tableName )
+    .insert( sampledata )
+    .then(function(data){
+      data = JSON.stringify(data);
+      console.log( data );
+    },
+    function(error){
+      console.log( error );
+    });
+
+
+
+  });
+
+})
+
+
+
+
+
+// module.exports = function ( grunt ) {
+//   grunt.registerTask('dbsetup', function( targetEnvironment ) {
+
+
+//     var done = this.async();
+//     done();
+
+//   });
+// }
